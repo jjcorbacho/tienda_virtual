6326b7d3b9e0feae43bf3e4d380916e598df059c
Julio Corbacho <70064635+jjcorbacho@users.noreply.github.com>
2021-02-08 14:49:25 -0500

    Trabajo Final

---
+++ frontend/src/utils.js
@@ -0,0 +1,43 @@
+export const prices = [
+  {
+    name: 'Any',
+    min: 0,
+    max: 0,
+  },
+  {
+    name: `S/1 to S/10`,
+    min: 1,
+    max: 10,
+  },
+  {
+    name: `S/10 to S/100`,
+    min: 10,
+    max: 100,
+  },
+  {
+    name: `S/100 to S/1000`,
+    min: 100,
+    max: 1000,
+  },
+];
+export const ratings = [
+  {
+    name: '4 estrellas',
+    rating: 4,
+  },
+
+  {
+    name: '3 estrellas',
+    rating: 3,
+  },
+
+  {
+    name: '2 estrellas',
+    rating: 2,
+  },
+
+  {
+    name: '1 estrella',
+    rating: 1,
+  },
+];
